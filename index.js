fb4a2ff683bd74761a23c83a0a94b9807b04f4ea
Matthew Grigajtis <matthew@Matthews-MacBook-Pro-2.local>
2014-06-16 22:52:58 -0400

    Removes simcolons, leaves percent completed

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var fs = require('fs');
-var torrents = require('torrent-stream');
-var path = require('path');
+var fs = require('fs')
+var torrents = require('torrent-stream')
+var path = require('path')
 
 module.exports = function (source, opts) {
-  "use strict";
-  var engine = torrents(source, opts);
+  var engine = torrents(source, opts)
 
   engine.on('ready', function () {
     engine.files.forEach(function(file) {
